3d516cc0018283f94c00b67b4eeba1424a097e10
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-03-31 21:01:02 +0100

    post student

--- routers/students.js
+++ routers/students.js
@@ -11,6 +11,20 @@ router.get('',async (req,res)=>{
     res.send(students);
 })
 
+router.post('',async (req,res)=>{
+    const student = new Student({
+        name : req.body.name,
+        age: req.body.age,
+        email: req.body.email
+    });
+    try{
+        const saved_student = await student.save();
+        res.status(201).send(saved_student);
+    }catch(err){
+        res.status(400).send(`Error : ${err.message}`);
+    }
+    
+})
 
 
 
