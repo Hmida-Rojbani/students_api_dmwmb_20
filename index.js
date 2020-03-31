8699a8cea8df04ab1fc7c53d7f8eff9f8a3f8fd0
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-03-31 20:08:33 +0100

    add express env

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const mongoose = require('mongoose');
 const appDebug = require('debug')('app:debug');
 const url_db = 'mongodb+srv://user:1234@mongoformation-xc16w.mongodb.net/dmwmB?retryWrites=true&w=majority';
 require('./db/connect')(url_db);
+const express = require('express');
+const morgan = require('morgan');
+const port = process.env.PORT | 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.listen(port,()=> appDebug(`Application is Running on ${port}`))
