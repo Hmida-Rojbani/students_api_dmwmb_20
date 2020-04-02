4742b9b214d0740180654472890fcec81e33b5e5
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-02 19:27:49 +0100

    add get bu id and get by name

--- models/student.js
+++ models/student.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectid = require('joi-objectid')(Joi);
 
 const student_schema = new mongoose.Schema({
     name : {type: String, require : true},
@@ -15,10 +16,31 @@ student_validator = {
     email: Joi.string().email().required()
 };
 
+student_opt_validator = {
+    name : Joi.string().min(3),
+    age: Joi.number().positive(),
+    email: Joi.string().email()
+};
+
+id_validator = {
+    id: Joi.objectid()
+};
+
 var student_not_valid_fun = (student) => {
     var results = Joi.validate(student,student_validator);
     return results.error;
 }
 
+var student_not_valid_opt_fun = (student) => {
+    var results = Joi.validate(student,student_opt_validator);
+    return results.error;
+}
+
+var id_not_valid_fun = (id) => {
+    var results = Joi.validate(id,id_validator);
+    return results.error;
+}
 module.exports.Student = Student;
 module.exports.student_not_valid_fun = student_not_valid_fun;
+module.exports.id_not_valid_fun = id_not_valid_fun;
+module.exports.student_not_valid_opt_fun = student_not_valid_opt_fun;
--- routers/students.js
+++ routers/students.js
@@ -1,15 +1,34 @@
 
 const router = require('express').Router();
-const { Student, student_not_valid_fun } = require('../models/student');
+const { Student, student_not_valid_fun, id_not_valid_fun, student_not_valid_opt_fun } = require('../models/student');
 const _ = require('lodash');
 
 
 router.get('',async (req,res)=>{
-    
     const students = await Student.find();
     if(students.length === 0)
         return res.status(204).end();
     res.send(students);
+})
+
+router.get('/id/:id',async (req,res)=>{
+    let results = id_not_valid_fun(req.params);
+    if(results)
+        return res.status(400).send(results.details[0].message);
+    const student = await Student.findById(req.params.id);
+    if(!student)
+        return res.status(204).end();
+    res.send(student);
+})
+
+router.get('/name/:name',async (req,res)=>{
+    let results = student_not_valid_opt_fun(req.params);
+    if(results)
+        return res.status(400).send(results.details[0].message);
+    const student = await Student.find({name:req.params.name});
+    if(!student)
+        return res.status(204).end();
+    res.send(student);
 })
 
 router.post('',async (req,res)=>{
