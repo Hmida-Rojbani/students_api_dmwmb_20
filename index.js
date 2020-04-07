751b7f49d88bc21f2a5bc447713aeb8274b405e2
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-04-07 18:37:19 +0100

    change to cloud again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const appDebug = require('debug')('app:debug');
-const url_db = 'mongodb://localhost:27017/dmwmB';
+const url_db = 'mongodb+srv://user:1234@mongoformation-xc16w.mongodb.net/dmwmB?retryWrites=true&w=majority';
 require('./db/connect')(url_db);
 const express = require('express');
 const morgan = require('morgan');
